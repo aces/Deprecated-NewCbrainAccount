179a17e02ab10c1be9a6a81512f9d617caa4c7e5
Pierre Rioux <pierre.rioux@mcgill.ca>
2013-03-26 17:08:37 -0400

    More secure authentication.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,22 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  helper :all
+  helper        :all # include all helpers, all the time
+
+  before_filter :verify_admin_token
+
+  def verify_admin_token
+    admin_user   = session[:admin]
+    unless admin_user
+      session.delete(:admin)
+      session.delete(:admin_token)
+      return
+    end
+    expect_token = AuthController.send(:admin_token_md5, admin_user)
+    return if session[:admin_token] == expect_token
+    session.delete(:admin)
+    session.delete(:admin_token)
+  end
 
 end
 
