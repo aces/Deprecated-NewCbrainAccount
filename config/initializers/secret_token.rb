accac7f244ae88a6f389376402bd088efec59e54
Pierre Rioux <pierre.rioux@mcgill.ca>
2021-10-19 19:28:34 -0400

    Update secret_token.rb
    Just removed a dummy secret because GitGuardian detected it. There never was a security risk though.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-NewAccount::Application.config.secret_token = 'dab86c4beded3f8814bbdcf6693f809e2c2ff69e0bcd55a376d0a9c6b30a4f96f476dcafdbc9ff25b8aecab80bbe09c1d712542553874bb17500797bd2c78aff'
+NewAccount::Application.config.secret_token = SecretWasHere
